19293563424e628f27aea98e60140f946cc65fc4
gorj97 <gorj97@mail.ru>
2020-06-16 21:35:20 +0500

    threads

--- server.py
+++ server.py
@@ -3,27 +3,38 @@
 # vim:fileencoding=utf-8
 
 import socket, random, os
+import time
 from datetime import datetime
+from collections import deque
+import threading
 
-sock = socket.socket()
-sock.bind(('', 1111))
-sock.listen(1)
-conn, addr = sock.accept()
-conn.settimeout(60)
+def thread_fun(messages):
+    while True:
+        message = input()
+        messages.append(message)
 
-while True:
-    response = 0
-    message = input()
-    conn.send(message.encode())
+if __name__ == "__main__":
+    sock = socket.socket()
+    sock.bind(('', 1111))
+    sock.listen(1)
+    conn, addr = sock.accept()
+    conn.settimeout(60)
 
-    while not response:
-        try:
-            response = conn.recv(1024)
-            print(response.decode())
-        except socket.timeout:
-            print("Истекло время ожидания ответа")
-            print("Переотправка")
-            continue
+    messages = deque()
 
+    x = threading.Thread(target=thread_fun, args=(messages,))
+    x.start()
 
-
+    while True:
+        if len(messages) > 0:
+            response = 0
+            message = messages.popleft()
+            conn.send(message.encode())
+            while not response:
+                try:
+                    response = conn.recv(1024)
+                    print(response.decode())
+                except socket.timeout:
+                    print("Истекло время ожидания ответа")
+                    print("Переотправка")
+                    continue
